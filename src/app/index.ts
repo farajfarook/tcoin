2ad72105fdeb3604d0e4e83cb99119e0cae2759b
Faraj Farook <farajfarook@gmail.com>
2021-10-22 22:14:02 +1100

    Adding P2p

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { Blockchain } from '../blockchain'
+import { P2PServer } from './p2p-server'
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
+const p2pServer = new P2PServer(bc)
 
 app.use(express.json())
 
@@ -32,3 +34,4 @@ app.post("/mine", (req, res) => {
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
+p2pServer.listen()
---
+++ src/app/p2p-server.ts
@@ -0,0 +1,35 @@
+import { Server, WebSocket } from "ws"
+import { Blockchain } from "../blockchain"
+
+const P2P_PORT = process.env.P2P_PORT ? parseInt(process.env.P2P_PORT) : 5001
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
+
+export class P2PServer {
+    blockchain: Blockchain
+    sockets: WebSocket[] = []
+
+    constructor(blockchain: Blockchain) {
+        this.blockchain = blockchain
+    }
+
+    listen() {
+        const server = new Server({ port: P2P_PORT })
+        server.on('connection', socket => this.connectSocket(socket))
+
+        this.connectToPeers();
+
+        console.log(`Listening for P2P connections on: ${P2P_PORT}`)
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new WebSocket(peer)
+            socket.on('open', () => this.connectSocket(socket))
+        })
+    }
+
+    connectSocket(socket: WebSocket) {
+        this.sockets.push(socket)
+        console.log('Socket connected')
+    }
+}
