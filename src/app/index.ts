b353273c5c7cb440e2d8a4fd4592526cc4dec75f
Faraj Farook <farajfarook@gmail.com>
2021-10-23 03:31:09 +1100

    p2p sync transaction

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,24 +1,35 @@
 import express from 'express'
 import { Blockchain } from '../blockchain'
+import { TransactionPool, Wallet } from '../wallet'
 import { P2PServer } from './p2p-server'
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
-const p2pServer = new P2PServer(bc)
+const wallet = new Wallet()
+const tp = new TransactionPool()
+const p2pServer = new P2PServer(bc, tp)
 
 app.use(express.json())
 
 app.get("/", (_, res) => {
     res.json({
-        "blocks": {
+        "list blocks": {
             "method": 'get',
             "url": "/blocks"
         },
-        "mine": {
+        "mine block": {
             "method": 'post',
-            "url": "/mine"
+            "url": "/blocks"
+        },
+        "list transaction": {
+            "method": 'get',
+            "url": "/transactions"
+        },
+        "add transaction": {
+            "method": 'post',
+            "url": "/transactions"
         }
     })
 })
@@ -27,13 +38,24 @@ app.get("/blocks", (_, res) => {
     res.json(bc.chain)
 })
 
-app.post("/mine", (req, res) => {
+app.post("/blocks", (req, res) => {
     const block = bc.addBlock(req.body.data)
     console.log(`New block added: ${block.toString()}`)
 
     p2pServer.syncChains()
 
     res.json(bc.chain)
+})
+
+app.get("/transactions", (_, res) => {
+    res.json(tp.transactions)
+})
+
+app.post("/transactions", (req, res) => {
+    const { recipient, amount } = req.body
+    const transaction = wallet.createTransaction(recipient, amount, tp)
+    p2pServer.broadcastTransaction(transaction)
+    res.json(tp.transactions)
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
--- src/app/p2p-server.ts
+++ src/app/p2p-server.ts
@@ -1,15 +1,21 @@
 import { Server, WebSocket } from "ws"
 import { Blockchain } from "../blockchain"
+import { Transaction, TransactionPool } from "../wallet"
 
 const P2P_PORT = process.env.P2P_PORT ? parseInt(process.env.P2P_PORT) : 5001
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
-
+const MESSAGE_TYPES = {
+    chain: 'CHAIN',
+    transaction: 'TRANSACTION'
+}
 export class P2PServer {
     blockchain: Blockchain
+    transactionPool: TransactionPool
     sockets: WebSocket[] = []
 
-    constructor(blockchain: Blockchain) {
+    constructor(blockchain: Blockchain, transactionPool: TransactionPool) {
         this.blockchain = blockchain
+        this.transactionPool = transactionPool
     }
 
     listen() {
@@ -38,16 +44,36 @@ export class P2PServer {
 
     messageHandler(socket: WebSocket) {
         socket.on('message', message => {
-            const chain = JSON.parse(message.toString())
-            this.blockchain.replaceChain(chain)
+            const { type, data } = JSON.parse(message.toString())
+            switch (type) {
+                case MESSAGE_TYPES.chain:
+                    this.blockchain.replaceChain(data)
+                    break
+                case MESSAGE_TYPES.transaction:
+                    this.transactionPool.addOrUpdate(data)
+            }
         })
     }
 
     sendChain(socket: WebSocket) {
-        socket.send(JSON.stringify(this.blockchain.chain))
+        socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.chain,
+            data: this.blockchain.chain
+        }))
+    }
+
+    sendTransaction(socket: WebSocket, transaction: Transaction) {
+        socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.transaction,
+            data: transaction
+        }))
     }
 
     syncChains() {
         this.sockets.forEach(socket => this.sendChain(socket))
+    }
+
+    broadcastTransaction(transaction: Transaction) {
+        this.sockets.forEach(socket => this.sendTransaction(socket, transaction))
     }
 }
